f36139ec78e94dd2668a367a753d78d4cdb63716
Quincy <8ca53525@opayq.com>
2019-01-05 16:53:50 +1100

    Completed Chapter 2

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,22 @@
+from flask import render_template
 from app import myapp
 
 
 @myapp.route('/')
 @myapp.route('/index')
 def index():
-    return "Hello World"
+    user = {'username': 'Marquin'}
+
+    posts = [
+        {
+            'author': {'username': 'Marquin'},
+            'body': 'Its stormy in sydney'
+        },
+
+        {
+            'author': {'username': 'Caroline'},
+            'body': 'I like cheese'
+        }
+
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
