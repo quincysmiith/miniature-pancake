6a34eb598dbfb53562fdfdc732c123e5ae4706fb
Quincy <8ca53525@opayq.com>
2019-02-06 21:33:54 +1100

    Chapter 5

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,14 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
 
 myapp = Flask(__name__)
 myapp.config.from_object(Config)
+db = SQLAlchemy(myapp)
+migrate = Migrate(myapp, db)
+login = LoginManager(myapp)
+login.login_view = 'login'
 
-from app import routes
+from app import routes, models
